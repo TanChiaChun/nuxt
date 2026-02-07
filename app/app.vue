05398362819c8ab767e40c8d9749628892ea7a47
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2026-02-07 22:03:30 +0800

    Init app shell

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="min-h-screen">
+    <main>
+      <div class="mx-auto max-w-7xl px-4 py-6">
+      </div>
+    </main>
   </div>
 </template>
