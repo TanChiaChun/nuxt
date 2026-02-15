3a0a93f1b5399a29f39071992fe817dec2af1b7c
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2026-02-16 00:25:08 +0800

    Add button to New Productivity page

--- app/pages/productivities/index.vue
+++ app/pages/productivities/index.vue
@@ -1,5 +1,16 @@
+<script setup lang="ts">
+import { Plus } from 'lucide-vue-next'
+</script>
+
 <template>
-  <div class="mx-auto max-w-xl">
+  <div class="mx-auto max-w-xl flex flex-col gap-3">
+    <div class="flex justify-end">
+      <UiButton variant="outline" size="icon" class="rounded-full" as-child>
+        <NuxtLink to="/productivities/new">
+          <Plus />
+        </NuxtLink>
+      </UiButton>
+    </div>
     <ProductivityList />
   </div>
 </template>
