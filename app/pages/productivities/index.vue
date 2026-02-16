b08988a769c531f88dfe22bca293de10396b9668
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2026-02-16 09:46:22 +0800

    Refactor inline ProductivityList

--- app/pages/productivities/index.vue
+++ app/pages/productivities/index.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { Plus } from 'lucide-vue-next'
+
+const { data: productivities } = await useFetch('/api/productivities')
 </script>
 
 <template>
@@ -11,6 +13,14 @@ import { Plus } from 'lucide-vue-next'
         </NuxtLink>
       </UiButton>
     </div>
-    <ProductivityList />
+
+    <div class="flex flex-col gap-2">
+      <ProductivityItem
+        v-for="productivity in productivities"
+        :key="productivity.id"
+        :name="productivity.name"
+        :last-check="productivity.lastCheck"
+      />
+    </div>
   </div>
 </template>
