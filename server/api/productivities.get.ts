bee7651ee42dc22380e0ed17bd68914161794e19
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2026-02-17 20:58:46 +0800

    Add error handling to GET

--- server/api/productivities.get.ts
+++ server/api/productivities.get.ts
@@ -2,5 +2,14 @@ import { db } from '#server/db/client'
 import { productivitiesTable } from '#server/db/schema/productivities'
 
 export default defineEventHandler(async (event) => {
-  return await db.select().from(productivitiesTable)
+  try {
+    const productivities = await db.select().from(productivitiesTable)
+    return productivities
+  } catch (e) {
+    console.error(e)
+    throw createError({
+      status: 500,
+      statusText: 'Server Error',
+    })
+  }
 })
