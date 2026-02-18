18a1dffa8ad53d22fb96853cd8e7dd9a6550647e
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2026-02-18 17:49:36 +0800

    Return 400 response on validation failure

--- server/api/productivities.post.ts
+++ server/api/productivities.post.ts
@@ -3,6 +3,15 @@ import { productivitySchema } from '#shared/schemas/productivities'
 export default defineEventHandler(async (event) => {
   const result = await readValidatedBody(event, productivitySchema.safeParse)
 
+  if (!result.success) {
+    console.error(result.error.issues)
+    throw createError({
+      status: 400,
+      statusText: 'Validation Error',
+      data: result.error.issues,
+    })
+  }
+
   setResponseStatus(event, 201)
   return result.data
 })
