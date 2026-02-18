489f06a461f8c27cec38efca701737ae2452bb7e
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2026-02-18 15:45:14 +0800

    Handle fetch error with Alert

--- app/pages/productivities/new.vue
+++ app/pages/productivities/new.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useForm } from 'vee-validate'
 import { toTypedSchema } from '@vee-validate/zod'
+import { CircleAlert } from 'lucide-vue-next'
 import { productivitySchema } from '#shared/schemas/productivities'
 
 const { defineField, errors, handleSubmit, isSubmitting } = useForm({
@@ -12,35 +13,47 @@ const { defineField, errors, handleSubmit, isSubmitting } = useForm({
 
 const [name, nameAttrs] = defineField('name')
 
+const errorMessage = ref<string |null>(null)
 const onSubmit = handleSubmit(async (values) => {
-  await $fetch('/api/productivities', {
-    method: 'POST',
-    body: values,
-  })
+  try {
+    await $fetch('/api/productivities', {
+      method: 'POST',
+      body: values,
+    })
+  } catch {
+    errorMessage.value = 'Error creating Productivity'
+  }
 })
 </script>
 
 <template>
-  <form @submit="onSubmit">
-    <UiFieldGroup>
-      <UiField>
-        <UiFieldLabel for="name">Name</UiFieldLabel>
-        <UiInput
-          type="text"
-          id="name"
-          v-model="name"
-          v-bind="nameAttrs"
-          :aria-invalid="!!errors.name"
-        />
-        <UiFieldError v-if="!!errors.name" :errors="[errors.name]" />
-      </UiField>
+  <div class="flex flex-col gap-3">
+    <UiAlert v-if="errorMessage" variant="destructive">
+      <CircleAlert />
+      <UiAlertTitle>{{ errorMessage }}</UiAlertTitle>
+    </UiAlert>
+  
+    <form @submit="onSubmit">
+      <UiFieldGroup>
+        <UiField :data-invalid="!!errors.name">
+          <UiFieldLabel for="name">Name</UiFieldLabel>
+          <UiInput
+            type="text"
+            id="name"
+            v-model="name"
+            v-bind="nameAttrs"
+            :aria-invalid="!!errors.name"
+          />
+          <UiFieldError v-if="!!errors.name" :errors="[errors.name]" />
+        </UiField>
 
-      <UiField orientation="horizontal">
-        <UiButton type="submit" :disabled="isSubmitting">
-          <UiSpinner v-if="isSubmitting" />
-          Submit
-        </UiButton>
-      </UiField>
-    </UiFieldGroup>
-  </form>
+        <UiField orientation="horizontal">
+          <UiButton type="submit" :disabled="isSubmitting">
+            <UiSpinner v-if="isSubmitting" />
+            Submit
+          </UiButton>
+        </UiField>
+      </UiFieldGroup>
+    </form>
+  </div>
 </template>
