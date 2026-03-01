aaf2e698f72bbda6bfdad98cfd4846e0374d8620
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2026-03-02 07:40:51 +0800

    Create separate ProductivityFormPage component

---
+++ app/components/productivity/ProductivityFormPage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ArrowLeft } from 'lucide-vue-next'
+
+const props = defineProps<{ title: string }>()
+</script>
+
+<template>
+  <div class="flex flex-col gap-3">
+    <ProductivityHeader :title="props.title">
+      <template #prepend>
+        <UiButton variant="ghost" size="icon" class="rounded-full" as-child>
+          <NuxtLink to="/productivities">
+            <ArrowLeft />
+          </NuxtLink>
+        </UiButton>
+      </template>
+    </ProductivityHeader>
+
+    <ProductivityForm />
+  </div>
+</template>
--- app/pages/productivities/new.vue
+++ app/pages/productivities/new.vue
@@ -1,19 +1,3 @@
-<script setup lang="ts">
-import { ArrowLeft } from 'lucide-vue-next'
-</script>
-
 <template>
-  <div class="flex flex-col gap-3">
-    <ProductivityHeader title="New">
-      <template #prepend>
-        <UiButton variant="ghost" size="icon" class="rounded-full" as-child>
-          <NuxtLink to="/productivities">
-            <ArrowLeft />
-          </NuxtLink>
-        </UiButton>
-      </template>
-    </ProductivityHeader>
-
-    <ProductivityForm />
-  </div>
+  <ProductivityFormPage title="New" />
 </template>
