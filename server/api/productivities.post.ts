5d6c36cc1cc209cc2aae19868b81f180390e3d6a
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2026-03-08 18:46:58 +0800

    Create getBody util func

--- server/api/productivities.post.ts
+++ server/api/productivities.post.ts
@@ -2,21 +2,9 @@ import { ProductivityPostSchema } from '#shared/schemas/productivities'
 import { createProductivity } from '#server/services/productivities.services'
 
 export default defineEventHandler(async (event) => {
-  const result = await readValidatedBody(
-    event,
-    ProductivityPostSchema.safeParse,
-  )
+  const body = await getBody(event, ProductivityPostSchema)
 
-  if (!result.success) {
-    console.error(result.error.issues)
-    throw createError({
-      status: 400,
-      statusText: 'Validation Error',
-      data: result.error.issues,
-    })
-  }
-
-  const inserted = await createProductivity(result.data)
+  const inserted = await createProductivity(body)
 
   setResponseStatus(event, 201)
   return inserted
--- server/utils/validate.ts
+++ server/utils/validate.ts
@@ -1,6 +1,24 @@
 import { z } from 'zod'
 import type { H3Event } from '#imports'
 
+export async function getBody<T extends z.ZodTypeAny>(
+  event: H3Event,
+  schema: T,
+): Promise<z.infer<T>> {
+  const result = await readValidatedBody(event, schema.safeParse)
+
+  if (!result.success) {
+    console.error(result.error.issues)
+    throw createError({
+      status: 400,
+      statusText: 'Validation Error',
+      data: result.error.issues,
+    })
+  }
+
+  return result.data
+}
+
 export async function getRouterParamId(event: H3Event) {
   const params = await getValidatedRouterParams(
     event,
