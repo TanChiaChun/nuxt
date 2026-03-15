b38d498efeece574e402dec32a4ce36acd29dd7c
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2026-03-16 07:17:30 +0800

    Show indicator if updating in progress

--- app/components/productivity/ProductivityItem.vue
+++ app/components/productivity/ProductivityItem.vue
@@ -8,6 +8,7 @@ const { updateProductivityLastCheck } = useProductivity()
 
 const props = defineProps<{ productivity: ProductivityResponse }>()
 
+const isUpdating = ref(false)
 const lastCheck = shallowRef(new Date(props.productivity.lastCheck))
 
 const now = useNow({ scheduler: cb => useIntervalFn(cb, 1000) })
@@ -24,6 +25,8 @@ const borderClass = computed(() => {
 })
 
 async function onUpdate() {
+  isUpdating.value = true
+
   const newLastCheck = new Date()
   try {
     await updateProductivityLastCheck(props.productivity.id,  newLastCheck)
@@ -33,6 +36,8 @@ async function onUpdate() {
     toast.success(`${props.productivity.name} updated`)
   } catch {
     toast.error(`Error updating ${props.productivity.name}`)
+  } finally {
+    isUpdating.value = false
   }
 }
 </script>
@@ -60,8 +65,13 @@ async function onUpdate() {
           <Pencil />
         </NuxtLink>
       </UiButton>
-      <UiButton size="icon-sm" variant="outline" @click="onUpdate">
-        <RefreshCcw />
+      <UiButton
+        size="icon-sm"
+        variant="outline"
+        @click="onUpdate"
+        :disabled="isUpdating"
+      >
+        <RefreshCcw :class="{ 'animate-spin': isUpdating }" />
       </UiButton>
     </UiItemActions>
   </UiItem>
