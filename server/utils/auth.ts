93669d40756a91dd2968cf7c7041587a613c82bf
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2026-03-22 23:33:54 +0800

    Set up Better Auth

---
+++ server/utils/auth.ts
@@ -0,0 +1,8 @@
+import { betterAuth } from 'better-auth/minimal'
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { db } from '#server/db/db'
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, { provider: 'pg' }),
+  emailAndPassword: { enabled: true },
+})
