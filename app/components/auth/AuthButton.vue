e97bb0f5ca3fb491ebd2f2fc2d53e58d7e2e4829
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2026-03-23 20:38:03 +0800

    Client sign out

--- app/components/auth/AuthButton.vue
+++ app/components/auth/AuthButton.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
+import { toast } from 'vue-sonner'
+
 const session = authClient.useSession()
+
+async function signOut() {
+  await authClient.signOut({
+    fetchOptions: {
+      onSuccess: async () => {
+        toast.success('Successfully log out')
+        await navigateTo('/login')
+      },
+      onError: () => {
+        toast.error('Error logging out')
+      }
+    }
+  })
+}
 </script>
 
 <template>
-  <UiButton v-if="session.data">Log Out</UiButton>
+  <UiButton v-if="session.data" @click="signOut">Log Out</UiButton>
   <UiButton v-else as-child>
     <NuxtLink to="/login">Log In</NuxtLink>
   </UiButton >
