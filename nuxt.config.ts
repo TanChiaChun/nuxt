10a19a8338de1de1f70a8c84824c1a576018c1a8
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2026-03-25 16:45:32 +0800

    Set HTML head title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ import tailwindcss from "@tailwindcss/vite"
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  app: {
+    head: {
+      title: 'Productivity',
+    },
+  },
   css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
   vite: {
