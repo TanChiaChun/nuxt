cc8379eedb4a156cbb4751b2780a0ea8d6ee32b0
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2026-04-04 20:51:55 +0800

    Set dynamic Better Auth base URL

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -1,9 +1,20 @@
+import 'dotenv/config'
 import { betterAuth } from 'better-auth/minimal'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { db } from '#server/db/db'
 import { user, account, session } from '#server/db/schema/auth-schema'
 
+function getBaseUrl() {
+  if (process.env.BETTER_AUTH_URL) {
+    return process.env.BETTER_AUTH_URL
+  } else if (process.env.VERCEL_BRANCH_URL) {
+    return `https://${process.env.VERCEL_BRANCH_URL}`
+  }
+  return 'http://localhost:3000'
+}
+
 export const auth = betterAuth({
+  baseURL: getBaseUrl(),
   database: drizzleAdapter(db, {
     provider: 'pg',
     schema: { user, account, session },
